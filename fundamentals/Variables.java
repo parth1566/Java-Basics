9e15adb1be234b452bf408bc9b295b88b889d314
Parth Sharma <parthsh.161106@gmail.com>
2026-06-24 16:33:57 +0530

    created Variables.java

---
+++ fundamentals/Variables.java
@@ -0,0 +1,12 @@
+package fundamentals;
+
+public class Variables {
+
+    public static void main(String[] args) {
+        String name = "Parth";
+        System.out.println(name);
+        System.out.println(name.length());
+
+    }
+    
+}
