52d8dbcfd40b876c3789e6cb2b8dc50463ed6e74
Parth Sharma <parthsh.161106@gmail.com>
2026-07-14 22:04:18 +0530

    ArrayListsQues

--- ArrayListsQues/RemovingDuplicates.java
+++ ArrayListsQues/RemovingDuplicates.java
@@ -1,5 +1,25 @@
 package ArrayListsQues;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class RemovingDuplicates {
-    
+    public static void NoDuplicates(ArrayList<Integer> inputArray) {
+        for(int i = 0; i < inputArray.size(); i++) {
+            for(int j = i + 1; j < inputArray.size(); j++){
+                if(inputArray.get(i).equals(inputArray.get(j))) {
+                    inputArray.remove(j);
+                    j--;
+                }
+            }
+        }
+        System.out.println(inputArray);
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(3, 5, 3, 1, 5, 7));
+        ArrayList<Integer> arr2 = new ArrayList<>(Arrays.asList(300, 5, 300));
+        NoDuplicates(arr);
+        NoDuplicates(arr2);
+    }
 }
+//[3, 5, 3, 1, 5, 7] 
